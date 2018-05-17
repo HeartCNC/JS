61a33bd450b5ef1ed5e024d872a43210101627a6
HeartCNC <32353232+HeartCNC@users.noreply.github.com>
2018-05-17 21:25:13 +0800

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -34,3 +34,19 @@ function data(){
 	}
 	
 }
+function page(total,now,limit){
+	var pages = Math.ceil(total/limit);
+	var str = "";
+	for(var i=1;i<=pages;i++){
+		str += "<a class='page'";
+		if( i != now ){
+			str += " data-page='"+i+"'";
+		}
+		str += ">"+i+"</a>";
+	}
+	$("#page").html(str);
+	$(".page").click(function(){
+		var t = $(this).attr("data-page");
+		page(total,t,limit);
+	});
+}
